6cc309e959fb132590055b1e282be18d8bb42fd3
legacycodine <adebimpee842@gmail.com>
2024-01-29 18:19:52 +0100

    Create object-export.js

---
+++ object-export.js
@@ -0,0 +1,11 @@
+const userlog = {
+  login: function () {
+    console.log("user login successfully");
+  },
+  loggout: function() {
+    console.log("user log out successfully");
+
+  },
+};
+
+module.exports = userlog
